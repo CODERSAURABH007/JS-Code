e1e5f6330aa23d1bab952d7dcab1b38c49f6426a
SAURABH KUMAR SINGH <116949691+CODERSAURABH007@users.noreply.github.com>
2024-05-25 08:46:28 +0000

    var

---
+++ start/var.js
@@ -0,0 +1,11 @@
+const accountId = 20021
+let accountname = "Saurabh"
+var accountpass = "hello"
+
+
+//accountId=2001
+
+accountname="Lyptus"
+accountpass="Hii"
+
+console.table([accountId,accountname,accountpass])
